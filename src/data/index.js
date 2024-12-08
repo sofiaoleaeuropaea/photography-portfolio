f5a198bf74c70303b8ed852b789b09818098d6d5
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-12-08 20:19:20 +0000

    Add navigation items and project data arrays

---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+export const navItems = [
+  { id: 1, name: 'Work,', link: '/' },
+  { id: 2, name: 'About,', link: '/about' },
+  { id: 3, name: 'Contact', link: '/contacts' },
+];
+
+export const projects = [
+  { id: 1, number: '01', title: 'Irland', link: '#irland' },
+  { id: 2, number: '02', title: 'Açores', link: '#acores' },
+  { id: 3, number: '03', title: 'Marrocos', link: '#marrocos' },
+  { id: 4, number: '04', title: 'Portugal', link: '#portugal' },
+  { id: 5, number: '05', title: 'Cape Verde', link: '#capeverde' },
+  { id: 6, number: '06', title: 'Oslo', link: '#oslo' },
+  { id: 7, number: '07', title: 'Lost files', link: '#lostfiles' },
+];
