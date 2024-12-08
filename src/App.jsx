e5e4b3befb0d94c504c6d187a5d038cd289efb94
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-12-08 20:16:13 +0000

    Create routing for Home, About, and Contacts pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -12,6 +14,8 @@ const App = () => {
         <main>
           <Routes>
             <Route index element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contacts' element={<Contacts />} />
           </Routes>
         </main>
         <Footer />
