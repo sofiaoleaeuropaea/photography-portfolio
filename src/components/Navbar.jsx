c4e671517c07ab7aeac720706d99dc2bdad5b767
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-12-09 00:17:43 +0000

    Add background opacity to navbar open

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,9 +21,15 @@ const Navbar = () => {
   };
 
   return (
-    <div className='fixed w-screen top-0 left-0 p-7'>
-      <nav>
-        <div className='text-base flex justify-between sm:hidden'>
+    <div className='fixed w-screen top-0 left-0'>
+       <div
+        className={`z-40 fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-600 sm:hidden ${
+          menuMobile ? 'opacity-70' : 'opacity-0 pointer-events-none'
+        }`}
+        onClick={() => setMenuMobile(false)}
+      ></div>
+      <nav className='relative p-7 z-50'>
+        <div className='text-base flex justify-between sm:hidden max-sm:bg-white max-sm:p-4'>
           <h1 className='text-sm'>Sofia Oliveira</h1>
           <button
             className='block text-sm'
@@ -31,13 +37,13 @@ const Navbar = () => {
             {menuMobile ? 'Close' : 'Menu'}
           </button>
         </div>
-        <div className='max-sm:bg-white max-sm:pt-3 max-sm:pl-5 max-sm:mt-7'>
+        <div className='max-sm:bg-white max-sm:py-8 max-sm:pl-6 max-sm:mt-5'>
           <ul
             className={`${
               menuMobile ? 'flex' : 'hidden'
-            } flex-col text-3xl sm:flex sm:flex-row sm:gap-2 justify-center sm:text-base `} >
+            } flex-col text-3xl sm:flex sm:flex-row sm:gap-2 justify-center sm:text-base `}>
             {navItems.map((navItem) => (
-              <li key={navItem.id} >
+              <li key={navItem.id}>
                 <FlipLink
                   href={navItem.link}
                   isActive={activeLink === navItem.id}
@@ -51,6 +57,7 @@ const Navbar = () => {
           </ul>
         </div>
       </nav>
+  
     </div>
   );
 };
