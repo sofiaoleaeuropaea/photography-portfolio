c01b4a0df5d881520d737e9599c707d88c75caf3
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2025-01-05 23:47:22 +0000

    Add footer grid

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <h1 className='max-sm:hidden text-base text-center'>Sofia Oliveira</h1>;
+  return (
+    <footer className='sm:grid sm:grid-cols-7 sm:fixed bottom-0 w-full m-4'>
+      <div className='col-start-3 col-end-4'>
+        <h1 className='max-sm:hidden'>Sofia Oliveira</h1>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
